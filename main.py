25b4105181076a01e3792393327401243c7722b2
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2023-02-04 22:45:26 -0500

    Update main.py
    optimized

--- main.py
+++ main.py
@@ -7,88 +7,89 @@ import csv
 
 
 # gets all outgoing links on website with bs4
-def getLinks(url):
+def getData(url):
     result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser") # parses to html
+    text = soup.get_text()
 
-    links = []
+    len = 0
     safe = 0
 
     for link in soup.find_all('a'):
         href = link.get('href')
         if href and "http" in href:
             # print(href)
-            links.append(href)
+            len += 1
             if "https" in href: safe += 1
 
-
-    size = len(links)
     # print("Number of links: {0}".format(size))
     # print("Number of safe links (https): {0}".format(safe))
-    # print("Number of non-safe links (http): {0}".format(size - safe))
-    return [size, safe, size - safe]
+    # print("Number of non-safe links (http): {0}".format(size - safe)
 
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
 
+    return [len, safe, len - safe, text, sentiment]
+
+'''
 # gets all text on a website with bs4
 def getText(url):
     result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser") # parses to html
 
-    # gets all text from url
-    text = soup.get_text()
     # print(text)
     # print("Number of words: {0}".format(len(text.split())))
-    return text
+    return len(soup.get_text())
 
+'''
 
 # gets current status of website
 def getStatus(url):
     response = requests.get(url)
 
     if response.status_code == 200:
-        # print(f"Successful request. The website {url} is up and running.")
         return 1
     else:
-        # print(f"Request failed. The website {url} is either down or unavailable.")
         return 0
 
 
 # gets domain name of url
 def getTLD(url):
-    tld = get_tld(url)
     # print(f"TLD: {tld}")
-    return tld
+    return get_tld(url)
 
-
+'''
 # checks sentiment of a text using textblob
 def sentiment(url):
     text = getText(url)
     blob = TextBlob(text)
     sentiment = blob.sentiment.polarity
-
-
-    '''
+    
     if sentiment > 0:
         print("Positive")
     elif sentiment == 0:
         print("Neutral")
     else:
         print("Negative")
-    '''
+
    
     return sentiment
-
+'''
+    
 
 def runAll(url):
-    links = getLinks(url)
-    totalLinks = links[0]
-    safeLinks = links[1]
-    nonSafeLinks = links[2]
+    try: getStatus(url)
+    except Exception: return
+   
+    data = getData(url)
+    totalLinks = data[0]
+    safeLinks = data[1]
+    nonSafeLinks = data[2]
 
-    text = getText(url)
+    text = data[3]
     status = getStatus(url)
     tld = getTLD(url)
-    senti = sentiment(url)
+    senti = data[4]
 
     # name of the file
     filename = "data.csv"
@@ -101,9 +102,6 @@ def runAll(url):
     # field names for the header
     fieldnames = ['url', 'url_length', 'https', 'total_links', 'safe_links', 'non_safe_links', 'length', 'status', 'tld', 'sentiment']
 
-    # flag to indicate if the header has already been written
-    header_written = True
-
     with open(filename, 'a', newline='') as file:
         # create a CSV writer object
         writer = csv.DictWriter(file, fieldnames=fieldnames)
@@ -113,7 +111,7 @@ def runAll(url):
 
 
 filename = "input.csv"
-column_index = 0
+column_index = 1
 with open(filename, 'r') as file:
     # create a CSV reader object
     reader = csv.reader(file)
@@ -124,7 +122,6 @@ with open(filename, 'r') as file:
     index = 0
     # iterate over the rows in the file
     for row in reader:
-        if index == 2: break
         # add the value in the specified column to the list
         runAll(row[column_index])
         index += 1
